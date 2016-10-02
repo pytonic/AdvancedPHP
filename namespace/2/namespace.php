901d45728498568474a6823efabe33be55247b27
pytonic <pytonic@foxmail.com>
2016-10-02 09:59:48 +0800

    2 3

---
+++ namespace/2/namespace.php
@@ -0,0 +1,42 @@
+<?php
+/*
+  +----------------------------------------------------------------------+
+  | Advanced PHP|
+  +----------------------------------------------------------------------+
+  | Copyright (c) 2016-2016 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author:  Yongtao Pang <pytonic@foxmail.com>                            |
+  +----------------------------------------------------------------------+
+*/
+namespace foo;
+include 'blah.php';
+use blah\blah as foo;
+class name{
+	public function f(){
+		echo 'foo\name' . "\n";
+	}
+}
+
+
+
+$a = new name(); // instantiates "foo\name" class
+$a->f();
+foo::name(); // calls static method "name" in class "blah\blah"
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
+
